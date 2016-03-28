5a99c14656b0ffc2a884f3f71ba2b87d5dce3ff2
Arkioner Sesko <Arkioner Sesko>
2016-03-28 13:47:38 +0200

    init :D

---
+++ Assets/Scripts/WaterTap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterTap : MonoBehaviour {
+
+	public GameObject waterParticle;
+
+	private int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnWater", 0, 0.01f);
+	}
+
+	public void SpawnWater() {
+		GameObject w = Instantiate (waterParticle);
+		w.transform.parent = transform;
+		float x = Random.Range (-300, 300) / 100f;
+		w.transform.position = w.transform.position + new Vector3(x, 0, 0);
+		count++;
+		if (count > 2000) {
+			CancelInvoke ();
+		}
+	}
+}
